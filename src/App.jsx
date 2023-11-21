ad76400fbb76966a35d82f9740d5c0ca7f2c4f6a
unknown <interestingshort787@gmail.com>
2023-11-21 10:00:41 +0500

    sdsdfsdf

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useRef,useState } from "react";
+import Emoji from "./emoji1/emoji";
+
+
+const App = () => {
+
+
+  return (
+  <>
+  <Emoji/>
+  </>
+  
+  );
+};
+
+export default App;
